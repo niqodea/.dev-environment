77d97375bdb34080c0563cb307aedf8dbe3a4725
Nicola De Angeli <nicoladeangeli@protonmail.com>
2022-10-16 23:51:46 +0200

    feat: add submodule-based fzf setup

--- setup/debian_install.sh
+++ setup/debian_install.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Note: For this to work you must have cloned the github repo to your home folder as ~/dotfiles
+
+repo_dir=dotfiles
+submodules_dir=submodules
+submodules_path=~/$repo_dir/$submodules_dir
+
 # Install neovim
 sudo apt-get --yes install neovim
 
@@ -8,4 +14,8 @@ sudo apt-get --yes install zsh
 # Make it default shell
 # Ref: https://askubuntu.com/a/131838
 sudo chsh -s $(which zsh)
+
+# Install fzf
+# The .zshrc in this repo already sources ~/.fzf.zsh if it exists, so no rc update is required
+$submodules_path/fzf/install --key-bindings --completion --no-update-rc --no-bash
 
