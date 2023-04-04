c8bec99d26a36d5b2ef0bae336b5caa2bc87d745
Nicola De Angeli <112023843+niqodea@users.noreply.github.com>
2023-04-04 20:16:57 +0200

    refactor: rearrange code

--- setup/debian_install.sh
+++ setup/debian_install.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
-# Note: For this to work you must have cloned the github repo to your home folder as ~/dotfiles
-
-repo_dir=dotfiles
-submodules_dir=submodules
-submodules_path=~/$repo_dir/$submodules_dir
-
-# Install neovim
-sudo apt-get --yes install neovim
+# Installing programs with apt-get
+# We use apt-get for programs that are not trivial to install via a script
 
 # Install Z shell
 sudo apt-get --yes install zsh
 # Make it default shell
 # Ref: https://askubuntu.com/a/131838
 sudo chsh -s $(which zsh)
+
+# Install neovim
+sudo apt-get --yes install neovim
+
+# Installing programs in the submodules directory
+# We use submodules for programs that offer a simple installation script
+
+# Note: For this to work you must have cloned the github repo to your home folder as ~/dotfiles
+repo_dir=dotfiles
+submodules_dir=submodules
+submodules_path=~/$repo_dir/$submodules_dir
 
 # Pull submodules
 git submodule update --recursive $submodules_path
