65fa88fc8196082745e9ebc9f93312bb59ca727d
Nicola De Angeli <nd@bendingspoons.com>
2023-08-22 23:37:08 +0200

    refactor: move git files to module

--- setup/install.sh
+++ setup/install.sh
@@ -11,11 +11,3 @@ echo "Pulling submodules..."
 core_submodules_path=$repo_root/submodules/core
 git submodule update --init --recursive $core_submodules_path
 
-echo "Compiling and installing git..."
-cd $core_submodules_path/git
-sudo apt install --yes --no-install-recommends asciidoc autoconf curl expat openssl perl python xmlto
-make configure
-./configure --prefix=$install_path
-# Do not localize Git and do not install gitk/git-gui
-make install install-man NO_GETTEXT=YesPlease NO_TCLTK=YesPlease
-
