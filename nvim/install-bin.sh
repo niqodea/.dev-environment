9f4f50bc49472b5a8bfdc8ce068a23ced3c5074b
Nicola De Angeli <112023843+niqodea@users.noreply.github.com>
2023-08-23 02:14:44 +0200

    fix: install nvim make dependency

--- nvim/install-bin.sh
+++ nvim/install-bin.sh
@@ -9,7 +9,7 @@ echo "Compiling and installing nvim..."
 src_path=$(dirname $0)/src
 git submodule update --init $src_path
 cd $src_path
-sudo apt install --yes --no-install-recommends cmake curl gettext ninja-build unzip
+sudo apt install --yes --no-install-recommends cmake curl gettext libtool-bin ninja-build pkg-config unzip
 make CMAKE_BUILD_TYPE=Release CMAKE_INSTALL_PREFIX=$install_path
 make install
 
