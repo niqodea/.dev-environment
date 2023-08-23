313875c488b76514869c4253e5ccdb96efff8852
Nicola De Angeli <112023843+niqodea@users.noreply.github.com>
2023-08-23 02:13:29 +0200

    fix: correctly pull submodules

--- git/install-bin.sh
+++ git/install-bin.sh
@@ -7,7 +7,7 @@ install_path=$1
 echo "Compiling and installing git..."
 
 src_path=$(dirname $0)/src
-git submodule update $src_path
+git submodule update --init $src_path
 cd $src_path
 sudo apt install --yes --no-install-recommends asciidoc autoconf curl expat openssl perl python xmlto
 make configure
--- nvim/install-bin.sh
+++ nvim/install-bin.sh
@@ -7,7 +7,7 @@ install_path=$1
 echo "Compiling and installing nvim..."
 
 src_path=$(dirname $0)/src
-git submodule update $src_path
+git submodule update --init $src_path
 cd $src_path
 sudo apt install --yes --no-install-recommends cmake curl gettext ninja-build unzip
 make CMAKE_BUILD_TYPE=Release CMAKE_INSTALL_PREFIX=$install_path
