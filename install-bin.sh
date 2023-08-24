8c9e13eb0faf64ec8970cf874ff5ee7b36271d3b
Nicola De Angeli <112023843+niqodea@users.noreply.github.com>
2023-08-24 10:09:05 +0200

    refactor: move .profile to .other-dotfiles

--- install-bin.sh
+++ install-bin.sh
@@ -5,25 +5,27 @@ set -eu
 cd $(dirname $0)/.gitroot
 
 # Reorder arguments to first process options
-args=$(getopt -o p: -n "\\$0" -- "$@")
+args=$(getopt -o ap: -n "\\$0" -- "$@")
 eval set -- $args
 
 # Default option values
+install_all_modules=false
 install_path=$HOME/.local
 
 while [ $1 != "--" ]; do
     case $1 in
+        -a) install_all_modules=true; shift 1;;
         -p) install_path="$2"; shift 2;;
         *) >&2 echo "Unhandled option: $1"; exit 1;;
     esac
 done
 shift
 
-if [ -n "${1+x}" ]; then
-    modules=$1
-else
-    # Consider all modules, i.e. all directories in repo root
+if [ $install_all_modules = true ]; then
+    # Get all non-dot subdirectories in root as a comma-separated string
     modules=$(ls -dm */ | tr -d "/" | tr -d " ")
+else
+    modules=$1
 fi
 
 IFS=","
