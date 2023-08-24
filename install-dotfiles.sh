7411728ee0c0ffe2b35180925aef34a2db147ad8
Nicola De Angeli <nicoladeangeli@protonmail.com>
2023-08-24 10:09:05 +0200

    refactor: move .profile to .other-dotfiles

--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -4,26 +4,42 @@ set -eu
 
 cd $(dirname $0)/.gitroot
 
-if [ -n "${1+x}" ]; then
-    modules=$1
-else
-    # Consider all modules, i.e. all directories in repo root
+# Reorder arguments to first process options
+args=$(getopt -o amo -n "\\$0" -- "$@")
+eval set -- $args
+
+# Default option values
+install_all_modules=false
+install_other=false
+
+while [ $1 != "--" ]; do
+    case $1 in
+        -a) install_other=true; install_all_modules=true; shift 1;;
+        -m) install_all_modules=true; shift 1;;
+        -o) install_other=true; shift 1;;
+        *) >&2 echo "Unhandled option: $1"; exit 1;;
+    esac
+done
+shift
+
+if [ $install_all_modules = true ]; then
+    # Get all non-dot subdirectories in root as a comma-separated string
     modules=$(ls -dm */ | tr -d "/" | tr -d " ")
+else
+    modules=${1+$1}  # empty string if unset
 fi
 
 backup_path=$HOME/dotfiles_backup_$(date +%Y%m%d_%H%M%S)
 
-IFS=","
-for module in $modules; do
-    source_dotfiles_path=$module/dotfiles
-    git submodule update --init --recursive $source_dotfiles_path
+install_dotfiles() {
+    source_dotfiles_path=$1
 
     if [ ! -d $source_dotfiles_path ]; then
-        >&2 echo "Dotfiles directory not found for module $module"
+        >&2 echo "Dotfiles directory does not exist: $source_dotfiles_path"
         exit 1
     fi
-    
-    echo "Installing dotfiles for module $module"
+
+    git submodule update --init --recursive $source_dotfiles_path
 
     for source_dotfile_path in $source_dotfiles_path/.[!.]*; do
         target_dotfile_relpath=$(basename $source_dotfile_path | sed "s/%2F/\//g")
@@ -42,12 +58,31 @@ for module in $modules; do
             rsync --archive --backup --backup-dir=$backup_dir \
                 $source_dotfile_path $HOME/$target_dotfile_relpath
         fi
-
     done
+}
+
+IFS=","
+for module in $modules; do
+    source_dotfiles_path=$module/dotfiles
+
+    if [ ! -d $source_dotfiles_path ]; then
+        >&2 echo "Dotfiles directory not found for module $module"
+        exit 1
+    fi
+    
+    echo "Installing dotfiles for module $module"
+
+    install_dotfiles $source_dotfiles_path
 
 done
 
+if [ $install_other = true ]; then
+    echo "Installing other dotfiles"
+
+    install_dotfiles .other-dotfiles
+fi
+
 if [ -e $backup_path ]; then
-	echo "Backed up overwritten files in $backup_path"
+    echo "Backed up overwritten files in $backup_path"
 fi
 
