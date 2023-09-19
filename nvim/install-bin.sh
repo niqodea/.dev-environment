e4022103edaeacaab3be911b11ace78e12550966
Nicola De Angeli <112023843+niqodea@users.noreply.github.com>
2023-09-20 00:29:59 +0200

    chore: bump nvim to v0.9.0

--- nvim/install-bin.sh
+++ nvim/install-bin.sh
@@ -10,6 +10,6 @@ src_path=$(dirname $0)/src
 git submodule update --init $src_path
 cd $src_path
 sudo apt install --yes --no-install-recommends cmake curl gettext libtool-bin ninja-build pkg-config unzip
-make CMAKE_BUILD_TYPE=Release CMAKE_INSTALL_PREFIX=$install_path
+make CMAKE_BUILD_TYPE=Release CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=$install_path"
 make install
 
