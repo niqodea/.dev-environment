21c722ee6360345570554721814119b5a84c0841
Nicola De Angeli <112023843+niqodea@users.noreply.github.com>
2023-09-23 01:18:47 +0200

    fix: remove buffy color from fzf-lua git grep

--- nvim/dotfiles/.config%2Fnvim/lua/plugins/dev/fzf-lua.lua
+++ nvim/dotfiles/.config%2Fnvim/lua/plugins/dev/fzf-lua.lua
@@ -7,19 +7,19 @@ vim.api.nvim_set_keymap(
 vim.api.nvim_set_keymap(
     'n',
     vim.g.mapleader..'fgr',
-    '<cmd>lua require("fzf-lua").live_grep({ cmd = "git grep --line-number --column --color=always" })<CR>',
+    '<cmd>lua require("fzf-lua").live_grep({ cmd = "git grep --line-number --column" })<CR>',
     {noremap = true}
 )
 vim.api.nvim_set_keymap(
     'n',
     vim.g.mapleader..'fgw',
-    '<cmd>lua require("fzf-lua").grep_cword({ cmd = "git grep --line-number --column --color=always" })<CR>',
+    '<cmd>lua require("fzf-lua").grep_cword({ cmd = "git grep --line-number --column" })<CR>',
     {noremap = true}
 )
 vim.api.nvim_set_keymap(
     'v',
     vim.g.mapleader..'fgr',
-    '<cmd>lua require("fzf-lua").grep_visual()<CR>',
+    '<cmd>lua require("fzf-lua").grep_visual({ cmd = "git grep --line-number --column" })<CR>',
     {noremap = true}
 )
 
