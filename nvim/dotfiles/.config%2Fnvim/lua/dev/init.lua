d8760af5cb56170fa31ee3b345d701d2d6b7fe0c
Nicola De Angeli <112023843+niqodea@users.noreply.github.com>
2023-10-24 13:12:40 +0200

    refactor: use better nvim comment line mapping

--- nvim/dotfiles/.config%2Fnvim/lua/dev/init.lua
+++ nvim/dotfiles/.config%2Fnvim/lua/dev/init.lua
@@ -5,7 +5,7 @@ vim.cmd('packadd nvim-comment')
 M.nvim_comment = require('nvim_comment')
 M.nvim_comment.setup{
     operator_mapping = vim.g.mapleader .. 'c',
-    line_mapping = vim.g.mapleader .. 'C',
+    line_mapping = vim.g.mapleader .. 'cc',
 }
 
 vim.cmd('packadd nvim-snippy')
