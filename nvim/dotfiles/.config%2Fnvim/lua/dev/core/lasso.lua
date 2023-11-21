5a968659790c42419f548c0aeadf39d8c45e056f
Nicola De Angeli <112023843+niqodea@users.noreply.github.com>
2023-11-21 08:48:21 +0100

    refactor: move num keybinds to fn

--- nvim/dotfiles/.config%2Fnvim/lua/dev/core/lasso.lua
+++ nvim/dotfiles/.config%2Fnvim/lua/dev/core/lasso.lua
@@ -6,12 +6,9 @@ M.lasso.setup{index_path = require('dev.core.utils').get_workspace_config_dir() 
 
 vim.api.nvim_set_keymap('n', vim.g.mapleader..'m', '<cmd>lua require("dev.core.lasso").lasso.mark_file()<cr>', {noremap = true})
 vim.api.nvim_set_keymap('n', vim.g.mapleader..'M', '<cmd>lua require("dev.core.lasso").lasso.open_index_file()<cr>', {noremap = true})
-vim.api.nvim_set_keymap('n', vim.g.mapleader..'1', '<cmd>lua require("dev.core.lasso").lasso.open_marked_file(1)<cr>', {noremap = true})
-vim.api.nvim_set_keymap('n', vim.g.mapleader..'2', '<cmd>lua require("dev.core.lasso").lasso.open_marked_file(2)<cr>', {noremap = true})
-vim.api.nvim_set_keymap('n', vim.g.mapleader..'3', '<cmd>lua require("dev.core.lasso").lasso.open_marked_file(3)<cr>', {noremap = true})
-vim.api.nvim_set_keymap('n', vim.g.mapleader..'4', '<cmd>lua require("dev.core.lasso").lasso.open_marked_file(4)<cr>', {noremap = true})
-vim.api.nvim_set_keymap('n', vim.g.mapleader..'<F1>', '<cmd>lua require("dev.core.lasso").lasso.open_terminal(1)<cr>', {noremap = true})
-vim.api.nvim_set_keymap('n', vim.g.mapleader..'<F2>', '<cmd>lua require("dev.core.lasso").lasso.open_terminal(2)<cr>', {noremap = true})
-vim.api.nvim_set_keymap('n', vim.g.mapleader..'<F3>', '<cmd>lua require("dev.core.lasso").lasso.open_terminal(3)<cr>', {noremap = true})
+vim.api.nvim_set_keymap('n', vim.g.mapleader..'<F1>', '<cmd>lua require("dev.core.lasso").lasso.open_marked_file(1)<cr>', {noremap = true})
+vim.api.nvim_set_keymap('n', vim.g.mapleader..'<F2>', '<cmd>lua require("dev.core.lasso").lasso.open_marked_file(2)<cr>', {noremap = true})
+vim.api.nvim_set_keymap('n', vim.g.mapleader..'<F3>', '<cmd>lua require("dev.core.lasso").lasso.open_marked_file(3)<cr>', {noremap = true})
+vim.api.nvim_set_keymap('n', vim.g.mapleader..'<F4>', '<cmd>lua require("dev.core.lasso").lasso.open_marked_file(4)<cr>', {noremap = true})
 
 return M
