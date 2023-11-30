f11480136917cc1ec8a659355108b877a2351298
Nicola De Angeli <112023843+niqodea@users.noreply.github.com>
2023-11-30 23:55:53 +0100

    refactor: add lasso nvim plugin as submodule

--- nvim/dotfiles/.config%2Fnvim/lua/dev/core/lasso.lua
+++ nvim/dotfiles/.config%2Fnvim/lua/dev/core/lasso.lua
@@ -1,11 +1,11 @@
 local M = {}
 
-vim.cmd('packadd lasso')
+vim.cmd('packadd lasso.nvim')
 M.lasso = require('lasso')
-M.lasso.setup{index_path = require('dev.core.utils').get_workspace_config_dir() .. '/lasso-index'}
+M.lasso.setup{marks_tracker_path = require('dev.core.utils').get_workspace_config_dir() .. '/lasso-marks-tracker'}
 
 vim.api.nvim_set_keymap('n', vim.g.mapleader..'m', '<cmd>lua require("dev.core.lasso").lasso.mark_file()<cr>', {noremap = true})
-vim.api.nvim_set_keymap('n', vim.g.mapleader..'M', '<cmd>lua require("dev.core.lasso").lasso.open_index_file()<cr>', {noremap = true})
+vim.api.nvim_set_keymap('n', vim.g.mapleader..'M', '<cmd>lua require("dev.core.lasso").lasso.open_marks_tracker()<cr>', {noremap = true})
 vim.api.nvim_set_keymap('n', vim.g.mapleader..'<F1>', '<cmd>lua require("dev.core.lasso").lasso.open_marked_file(1)<cr>', {noremap = true})
 vim.api.nvim_set_keymap('n', vim.g.mapleader..'<F2>', '<cmd>lua require("dev.core.lasso").lasso.open_marked_file(2)<cr>', {noremap = true})
 vim.api.nvim_set_keymap('n', vim.g.mapleader..'<F3>', '<cmd>lua require("dev.core.lasso").lasso.open_marked_file(3)<cr>', {noremap = true})
