a0c32ebe64af1e2ff2ca1d37bceda6bd6253f794
Nicola De Angeli <112023843+niqodea@users.noreply.github.com>
2023-12-02 13:44:24 +0100

    fix: address fzf install script annoyance

--- utils/install-bin.sh
+++ utils/install-bin.sh
@@ -13,7 +13,12 @@ cp $repo_path/breadcrumbs $install_path/bin/
 echo "Downloading and installing fzf..."
 repo_path=$(dirname $0)/bin-submodules/fzf
 git submodule update --init $repo_path
+# The fzf install script produces a symlink to the binary in the bin directory when fzf
+# is already installed. Since this makes it hard to update fzf by rerunning the install
+# script, we simply remove fzf from bin before running it.
+rm -f $install_path/bin/fzf
 $repo_path/install --bin
+# We do not copy so that new versions can be installed by rerunning the install script
 mv $repo_path/bin/fzf $install_path/bin/
 
 echo "Compiling and installing pass..."
