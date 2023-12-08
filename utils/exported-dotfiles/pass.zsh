c9a631592304ef54f43bb6e35543cdc3cbc43145
Nicola De Angeli <112023843+niqodea@users.noreply.github.com>
2023-12-09 00:07:30 +0100

    refactor: centralize password store

--- utils/exported-dotfiles/pass.zsh
+++ utils/exported-dotfiles/pass.zsh
@@ -1,16 +1,9 @@
-export PASSWORD_STORE_DIR="$ZSH_ROOT/.password-store"
+export PASSWORD_STORE_DIR="$HOME/.password-store"
 
 alias gpg-create-key='gpg --quick-generate-key "$USER" rsa3072 encr,sign 0'
 alias gpg-decrypt='echo Success | gpg -e -r "$USER" | gpg -d --pinentry-mode loopback'
 
 alias pass-init='pass init "$USER"'
-
-function pass-shortcut-setup() {
-    local shortcuts_path="$PASSWORD_STORE_DIR/shortcuts"
-    mkdir "$shortcuts_path"
-    # Set up breadcrumbs (ref: https://github.com/niqodea/breadcrumbs)
-    breadcrumbs -s "$PASSWORD_STORE_DIR" -t "$shortcuts_path" -n "password-store"
-}
 
 function pass-shortcut-add() {
     item=$1
@@ -20,17 +13,14 @@ function pass-shortcut-add() {
         return 1
     fi
 
-    local shortcuts_path="$PASSWORD_STORE_DIR/shortcuts"
-
-    shortcut_idx=1
-    while [ -e "$shortcuts_path/$shortcut_idx.gpg" ]; do
-        shortcut_idx="$((shortcut_idx + 1))"
-    done
-
-    ln -s ".password-store.bc/$item.gpg" "$shortcuts_path/$shortcut_idx.gpg"
+    local shortcuts_path="$ZSH_ROOT/.pass-shortcuts"
+    echo "$item" >> "$shortcuts_path"
 }
 
 function pass-shortcut-show() {
+    local shortcuts_path="$ZSH_ROOT/.pass-shortcuts"
+
     shortcut_idx="$1"
-    pass show "shortcuts/$shortcut_idx"
+    item=$(head -n "$shortcut_idx" "$shortcuts_path" | tail -n 1)
+    pass show "$item"
 }
