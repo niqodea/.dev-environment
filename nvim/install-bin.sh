a775299ba7a0b49fee6e1bbd34b56d9721568928
Nicola De Angeli <112023843+niqodea@users.noreply.github.com>
2023-12-25 18:44:32 +0100

    feat: install node js for copilot

--- nvim/install-bin.sh
+++ nvim/install-bin.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-# TODO add installation of nodejs v16.x+ for copilot.vim
-# (the version of nodejs in the apt repository is too old)
-
 set -eux
 
 root="$(dirname "$(realpath "$0")")"
@@ -16,3 +13,12 @@ cd "$repo_path"
 sudo apt install --yes --no-install-recommends cmake curl gettext libtool-bin ninja-build pkg-config unzip
 make CMAKE_BUILD_TYPE="Release" CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=\"$install_path\""
 make install
+
+# We need to install nodejs from the nodesource repository
+# This is because the version of nodejs in the apt repository is too old
+# Ref: https://deb.nodesource.com
+sudo apt install --yes --no-install-recommends ca-certificates curl gnupg
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_20.x nodistro main' | sudo tee /etc/apt/sources.list.d/nodesource.list
+sudo apt update
+sudo apt install --yes --no-install-recommends nodejs
