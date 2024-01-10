5b824b899f88f66097b7f8d7c624a731b90aace8
Nicola De Angeli <112023843+niqodea@users.noreply.github.com>
2024-01-10 22:57:51 +0100

    refactor: package binaries with python venv module

--- python/install-bin.sh
+++ python/install-bin.sh
@@ -14,22 +14,24 @@ sh "$conda_tmp_path" -b -p "$conda_install_path"
 rm "$conda_tmp_path"
 ln -s "$conda_install_path/bin/conda" "$install_path/bin/"
 
+sudo apt install --yes --no-install-recommends python3.11 python3.11-venv
+
 # Ref: https://python-poetry.org/docs/#installing-manually
 poetry_venv_path="$HOME/.poetry-venv"
-conda create --prefix "$poetry_venv_path" --yes python=3.8
+python3.11 -m venv "$poetry_venv_path"
 "$poetry_venv_path/bin/pip" install poetry==1.6.1
 ln -s "$poetry_venv_path/bin/poetry" "$install_path/bin/"
 
 # Ref: https://github.com/pappasam/jedi-language-server#installation
 jedi_venv_path="$HOME/.jedi-venv"
-conda create --prefix "$jedi_venv_path" --yes python=3.8
+python3.11 -m venv "$jedi_venv_path"
 "$jedi_venv_path/bin/pip" install jedi-language-server==0.41.1
 ln -s "$jedi_venv_path/bin/jedi-language-server" "$install_path/bin/"
 
 ropify_path="$root/bin-submodules/ropify"
 ropify_venv_path="$HOME/.ropify-venv"
 git submodule update --init "$ropify_path"
-conda create --prefix "$ropify_venv_path" --yes python=3.10
+python3.11 -m venv "$ropify_venv_path"
 "$ropify_venv_path/bin/pip" install "$ropify_path"
 ln -s "$ropify_venv_path/bin/ropify" "$install_path/bin/"
 
