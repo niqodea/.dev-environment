563f5217d296cc22c215d56661e073b501881377
Nicola De Angeli <112023843+niqodea@users.noreply.github.com>
2024-04-18 21:13:01 +0200

    refactor: move zsh functions to separate file

---
+++ zsh/dotfiles/.zsh-modules/core/functions.zsh
@@ -0,0 +1,36 @@
+function bkp () {
+    local file_path="$1"
+    if [ -z "$file_path" ]; then
+        >&2 echo "No path provided"
+        return 1
+    fi
+    if [ ! -e "$file_path" ]; then
+        >&2 echo "Path does not exist: $file_path"
+        return 1
+    fi
+
+    local backup_file_path="$file_path.bkp"
+    if [ -e "$backup_file_path" ]; then
+        bkp "$backup_file_path"
+    fi
+
+    mv -- "$file_path" "$backup_file_path"
+}
+
+function sw () {
+    local workstation="$1"
+
+    local ssh_command="ssh -t"  # -t to specify a command to run at the end
+    # Options for sturdier connections
+    ssh_command="$ssh_command -o ServerAliveInterval=60 -o ServerAliveCountMax=3"
+
+    for port in {50000..50019}; do
+        ssh_command="$ssh_command -L 0.0.0.0\:$port\:localhost\:$port"
+    done
+
+    ssh_command="$ssh_command $workstation"
+    # Connect to tmux, launching the server if necessary
+    ssh_command="$ssh_command 'tmux ls &> /dev/null && tmux attach || tmux new'"
+
+    sh -c "$ssh_command"
+}
