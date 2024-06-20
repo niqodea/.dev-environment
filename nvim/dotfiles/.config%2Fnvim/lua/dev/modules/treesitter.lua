7c1755f3dc1f50a26c75ad4336d6e3ebaa413315
Nicola De Angeli <112023843+niqodea@users.noreply.github.com>
2024-06-20 12:50:16 +0200

    fix: use vim regex highlighting too in nvim

--- nvim/dotfiles/.config%2Fnvim/lua/dev/modules/treesitter.lua
+++ nvim/dotfiles/.config%2Fnvim/lua/dev/modules/treesitter.lua
@@ -1,7 +1,15 @@
 vim.cmd('packadd nvim-treesitter')
 local nvim_treesitter_configs = require('nvim-treesitter.configs')
 
-nvim_treesitter_configs.setup { highlight = { enable = true } }
+nvim_treesitter_configs.setup {
+    highlight = {
+        enable = true,
+        -- Required for some color schemes to work properly with treesitter
+        -- Otherwise some tokens might not be highlighted
+        -- Ref: https://stackoverflow.com/questions/78220353
+        additional_vim_regex_highlighting = true,
+    },
+}
 
 -- Setup parsers for each language
 local language_files = vim.api.nvim_get_runtime_file('lua/dev/modules/treesitter-languages/*.lua', true)
