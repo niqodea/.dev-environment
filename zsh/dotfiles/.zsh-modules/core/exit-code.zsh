a335cd6cdffca8dd3ec6bec286a07568fb77f678
Nicola De Angeli <112023843+niqodea@users.noreply.github.com>
2024-06-22 00:10:23 +0200

    docs: add comments to exit code zsh module

--- zsh/dotfiles/.zsh-modules/core/exit-code.zsh
+++ zsh/dotfiles/.zsh-modules/core/exit-code.zsh
@@ -1,5 +1,6 @@
 ZSH_MONITOR_EXIT_CODE=0
 
+# Only monitor exit code if we execute a command
 function enable_monitor_exit_code() {
     ZSH_MONITOR_EXIT_CODE=1
 }
@@ -9,6 +10,7 @@ function monitor_exit_code() {
     exit_code="$?"  # capture it immediately or it will be lost
 
     if [ "$ZSH_MONITOR_EXIT_CODE" != 1 ]; then
+        # A command has not been executed, no exit code to monitor
         return
     fi
     ZSH_MONITOR_EXIT_CODE=0
