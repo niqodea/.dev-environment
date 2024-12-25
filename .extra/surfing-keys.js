449da86f95ccb7290258de653a7c4755ab65a828
Nicola De Angeli <112023843+niqodea@users.noreply.github.com>
2024-12-25 12:17:56 +0100

    feat: add surfing keys config

---
+++ .extra/surfing-keys.js
@@ -0,0 +1,7 @@
+api.map('K', 'E');  // previous tab
+api.map('J', 'R');  // next tab
+
+api.map('H', 'S');  // back in history
+api.map('L', 'D');  // forward in history
+
+api.map('<Space>q', 'x'); // close tab
