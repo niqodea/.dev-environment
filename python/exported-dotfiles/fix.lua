53546577b1bf03ec3414c77202483b533a24bdc4
Nicola De Angeli <112023843+niqodea@users.noreply.github.com>
2024-12-28 11:34:57 +0100

    feat: add python ruff nvim commands

--- python/exported-dotfiles/fix.lua
+++ python/exported-dotfiles/fix.lua
@@ -1,16 +1,27 @@
 return function(set_qflist, set_loclist)
-    local title = 'Mypy'
-    local mypy_base_command = 'mypy --show-column-numbers --no-error-summary '
 
+    local mypy_title = 'Mypy'
+    local mypy_base_command = 'mypy --show-column-numbers --no-error-summary '
     vim.api.nvim_create_user_command('PythonMypy', function()
         local file_path = vim.fn.expand('%:p')
         local command = mypy_base_command .. file_path
-        set_loclist(title, command)
+        set_loclist(mypy_title, command)
     end, {})
-
     vim.api.nvim_create_user_command('PythonMypyProject', function()
         local command = mypy_base_command .. vim.fn.getcwd()
-        set_qflist(title, command)
+        set_qflist(mypy_title, command)
+    end, {})
+
+    local ruff_title = 'Ruff'
+    local mypy_base_command = 'ruff check --output-format=concise '
+    vim.api.nvim_create_user_command('PythonRuff', function()
+        local file_path = vim.fn.expand('%:p')
+        local command = mypy_base_command .. file_path
+        set_loclist(ruff_title, command)
+    end, {})
+    vim.api.nvim_create_user_command('PythonRuffProject', function()
+        local command = mypy_base_command .. vim.fn.getcwd()
+        set_qflist(ruff_title, command)
     end, {})
 
 end
